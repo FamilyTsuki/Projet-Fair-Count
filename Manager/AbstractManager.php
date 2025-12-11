9b5534affd4ba87546c65d174cf28f1c15bdbfaf
Tsuki <alban.elie590@gmail.com>
2025-12-11 13:34:12 +0100

    style.css

--- Manager/AbstractManager.php
+++ Manager/AbstractManager.php
@@ -9,7 +9,21 @@ abstract class AbstractManager
 
     public function __construct()
     {
-        $this->pdo = Database::getConnection(); 
+        $host = "host de la base, généralement localhost";
+$port = "3306";
+$dbname = "nomdelabase";
+$connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+
+$user = "votre_username";
+$password = "votre_password";
+
+$db = new PDO(
+    $connexionString,
+    $user,
+    $password
+);
+
+
     }
     
     // ...

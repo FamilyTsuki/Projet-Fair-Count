09e0dfb6cc9e3e6703a56f5a9cd22adca25b2273
Tsuki <alban.elie590@gmail.com>
2025-12-11 13:35:49 +0100

    truc

--- Manager/AbstractManager.php
+++ Manager/AbstractManager.php
@@ -10,17 +10,17 @@ abstract class AbstractManager
     public function __construct()
     {
         $host = "host de la base, généralement localhost";
-$port = "3306";
-$dbname = "nomdelabase";
-$connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+        $port = "3306";
+        $dbname = "nomdelabase";
+        $connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
 
-$user = "votre_username";
-$password = "votre_password";
+        $user = "votre_username";
+        $password = "votre_password";
 
-$db = new PDO(
-    $connexionString,
-    $user,
-    $password
+        $this->$pdo = new PDO(
+            $connexionString,
+            $user,
+            $password
 );
 
 

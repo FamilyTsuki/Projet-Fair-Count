336442f3bb17c4ee76a1b3a1b73b51da576497b8
Tsuki <alban.elie590@gmail.com>
2025-12-12 14:10:10 +0100

    debut group manager

--- Manager/GroupeManager.php
+++ Manager/GroupeManager.php
@@ -9,7 +9,10 @@ class GroupeManager extends AbstractManager
     }
     public function getAllGroupe() : array
     {
-        $query = $this->db->prepare("SELECT id , name , budget, code FROM groupe");
+        $query = $this->db->prepare("SELECT groupe.id , groupe.name , groupe.budget, groupe.code FROM groupe
+                                            JOIN groupe_participants on groupe_participants.groupe_id = groupe.id 
+                                            JOIN users on groupe_participants.user_id = groupe.id
+                                            WHERE users.id = 4;");
         $query->execute();
         $results = $query->fetchAll(PDO::FETCH_ASSOC);
         $Groupes = [];

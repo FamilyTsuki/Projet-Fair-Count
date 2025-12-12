11a7f119a221c57adb970f48946e09c8918b677a
raphael <raphaelecerf@gmail.com>
2025-12-12 10:24:02 +0100

    ajout des groupe au model

--- Model/Reimbursements.php
+++ Model/Reimbursements.php
@@ -6,12 +6,14 @@ class Reimbursements {
     private $date;
     private $user_from_id;
     private $user_to_id;
-    public function __construct($id, $amount, $date, $user_from_id, $user_to_id) {
+    private $groupe_id;
+    public function __construct($id, $amount, $date, $user_from_id, $user_to_id, $groupe_id) {
         $this->id = $id;
         $this->amount = $amount;
         $this->date = $date;
         $this->user_from_id = $user_from_id;
         $this->user_to_id = $user_to_id;
+        $this->groupe_id = $groupe_id;
     }
     public function getId() {
         return $this->id;
@@ -27,5 +29,8 @@ class Reimbursements {
     }
     public function getUser_to_id() {
         return $this->user_to_id;
+    }
+    public function getGroupe_id() {
+        return $this->groupe_id;
     }
 }
